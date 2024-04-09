0a38aa0d677792afd0439b74de2ce85702e326a4
BohdanBoriak <lightraven72@gmail.com>
2024-04-09 11:30:05 +0300

    WIP game development

--- main.go
+++ main.go
@@ -1,7 +1,34 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"math/rand"
+	"time"
+)
+
+const totalPoints = 50
 
 func main() {
-	fmt.Println("Hello, World!")
+	fmt.Println("Вітаємо у грі MATHCORE!")
+	time.Sleep(1 * time.Second)
+
+	for i := 5; i > 0; i-- {
+		fmt.Println(i)
+		time.Sleep(1 * time.Second)
+	}
+
+	start := time.Now()
+	myPoints := 0
+	for myPoints < totalPoints {
+		x, y := rand.Intn(100), rand.Intn(100)
+		res := x + y
+		fmt.Println(x, "+", y, "=")
+		fmt.Printf("%v + %v = ", x, y)
+
+		ans := ""
+		fmt.Scan(&ans)
+		if ans == res {
+
+		}
+	}
 }
